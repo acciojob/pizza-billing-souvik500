d9b66de6a522af286eeeb8da7979bb232a97a679
souvikadhikary <souvik500>
2023-02-11 19:44:52 +0530

    PIZZA BILL

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,15 +4,15 @@ public class Main {
   public static void main(String[] args) {
     DeluxePizza dp = new DeluxePizza(true);
     System.out.println(dp.getPrice());
-    dp.addTakeaway();
+    dp.addExtraToppings();
     dp.addExtraCheese();
     dp.addTakeaway();
     System.out.println(dp.getBill());
 
-    Pizza p = new Pizza(true);
+    Pizza p = new Pizza(false);
     p.addExtraCheese();
     p.addExtraToppings();
-    p.addTakeaway();
+    //p.addTakeaway();
     System.out.println(p.getBill());
   }
 }
